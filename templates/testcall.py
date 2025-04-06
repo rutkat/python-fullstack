65789beb4d9239996fc9cd7ed80799244c2e7091
Rutkat <4261178+rutkat@users.noreply.github.com>
2025-04-06 19:21:59 -0400

    Update testcall.py

--- templates/testcall.py
+++ templates/testcall.py
@@ -1,15 +1,11 @@
 from openai import OpenAI
 
-#base_url = "https://api.aimlapi.com/v1"
 base_url = "http://localhost:1234/v1/"
  
-# Insert your AIML API key in the quotation marks instead of <YOUR_AIMLAPI_KEY>:
-api_key = "not-needed"
+system_prompt = "You are a learning assistant and teacher. Respond with accurate answers. If you don't know the answer, then be honest."
+user_prompt = "Help me learn how to use AI. Provide a learning outline."
 
-system_prompt = "You are a travel agent. Be descriptive and helpful."
-user_prompt = "Tell me about San Francisco"
-
-api = OpenAI(base_url=base_url, api_key=api_key)
+api = OpenAI(base_url=base_url, api_key="none")
 
 def main():
     completion = api.chat.completions.create(
@@ -26,7 +22,6 @@ def main():
 
     print("User:", user_prompt)
     print("AI:", response)
-
 
 if __name__ == "__main__":
     main()
